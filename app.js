cc1ea0079424be2c363442d40f2db9f27b1d3cc2
teckling <teckling.tan@yahoo.com>
2025-06-06 23:43:43 +0800

    add router, serve static image files

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path')
 
-app.use(express.static('public'))
+app.use(express.static('public'))         // you need this to make the css available to the browser
+app.use(express.static('images'))         // you need this to make the images available to the browser
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  res.render('index');
-});
+const defaultRoute = require('./routes/default');
+app.use('/', defaultRoute);
 
 app.listen(3000);
