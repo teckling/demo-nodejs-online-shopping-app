81fcfa90a05cb7fc22fec61781f4f5aa6794bd22
teckling <teckling.tan@yahoo.com>
2025-06-21 16:19:42 +0800

    create admin form with text and image upload

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 const defaultRoute = require('./routes/default');
+const adminRoute = require('./routes/admin');
 app.use('/', defaultRoute);
+app.use('/', adminRoute);                   // notice I used '/' here not '/admin' why?
 
 app.listen(3000);
